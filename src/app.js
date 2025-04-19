d0c7cde38d4041f272899291e9d3579fa773f7cb
Shreya Kushwaha <shreyakush013@gmail.com>
2025-04-19 19:32:08 +0530

    exploring fet,post,dlt

--- src/app.js
+++ src/app.js
@@ -3,13 +3,25 @@ const express = require("express")
 
 const app = express();
 
+app.get("/user",(req,res) =>{
+    res.send({firstname:"Shreya Kushwaha",lastname:"kushwaha"})
+})
+
+app.post("/user",(req,res) =>{
+    res.send("post request successfully done")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("delete request successfylly done")
+})
+
 // handling the code
-// app.use((req,res) =>{
-//     res.send("hello from thr server") ///this function handles the code
-// })
 app.use("/test",(req,res) =>{
     res.send("test server") ///this function handles the code
 })
+// app.use("/",(req,res) =>{
+//     res.send("hello from thr server") ///this function handles the code
+// })
 
 
 app.listen(7777, ()=>{
